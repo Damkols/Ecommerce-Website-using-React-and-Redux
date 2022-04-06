ea511ed056102d28785a446ef39493b1dcf3a0a6
damkols <damolausmankolapo@gmail.com>
2022-04-06 11:52:41 -0800

    built navbar

---
+++ src/components/Products.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+const Products = () => {
+  const [data, setData] = useState([]);
+  const [filter, setfilter] = useState([data]);
+  const [loading, setloading] = useState(false);
+
+  return <div></div>;
+};
+
+export default Products;
